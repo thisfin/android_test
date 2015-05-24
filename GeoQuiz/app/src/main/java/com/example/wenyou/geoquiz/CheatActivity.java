623f7ac6664215fff97f891a6211b52bfcf408b2
文优 <thisfin@gmail.com>
2015-05-24 20:07:21 +0800

    update

---
+++ GeoQuiz/app/src/main/java/com/example/wenyou/geoquiz/CheatActivity.java
@@ -0,0 +1,50 @@
+package com.example.wenyou.geoquiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by wenyou on 15/5/24.
+ */
+public class CheatActivity
+        extends Activity {
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.example.wenyou.geoquiz.answer_is_true";
+    public static final String EXTRA_ANSWER_SHOWN = "com.example.wneyou.geoquiz.answer_shown";
+
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_cheat);
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        setAnswerShownResult(false);
+
+        mAnswerTextView = (TextView)super.findViewById(R.id.answerTextView);
+        mShowAnswer = (Button)super.findViewById(R.id.showAnswerButton);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
+    }
+}
--- GeoQuiz/app/src/main/java/com/example/wenyou/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/wenyou/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.example.wenyou.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +19,7 @@ public class QuizActivity extends ActionBarActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
 
     private TrueFalse[] mQuestionBank = new TrueFalse[] {
@@ -28,6 +30,7 @@ public class QuizActivity extends ActionBarActivity {
             new TrueFalse(R.string.question_asia, true)
     };
     private int mCurrentIndex = 0;
+    private boolean mIsCheater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +68,19 @@ public class QuizActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mIsCheater = false;
                 QuizActivity.this.updateQuestion();
+            }
+        });
+
+        mCheatButton = (Button)super.findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].ismTrueQuestion();
+                i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+                startActivityForResult(i, 0);
             }
         });
     }
@@ -129,6 +144,14 @@ public class QuizActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {
+            return;
+        }
+        mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
+    }
+
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getmQuestion();
         mQuestionTextView.setText(question);
@@ -138,10 +161,14 @@ public class QuizActivity extends ActionBarActivity {
         boolean answerIsTrus = mQuestionBank[mCurrentIndex].ismTrueQuestion();
         int messageResId = 0;
 
-        if (userPressedTrue == answerIsTrus) {
-            messageResId = R.string.correct_toast;
+        if (mIsCheater) {
+            messageResId = R.string.judgment_toast;
         } else {
-            messageResId = R.string.incorrect_toast;
+            if (userPressedTrue == answerIsTrus) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
         }
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
